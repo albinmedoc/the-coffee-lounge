099e57d1621481889e5060f7fa8ade29f632dc6b
Albin Médoc <stamline203@gmail.com>
2020-08-14 23:43:29 +0200

    Created desserts page

---
+++ src/routes/fika.svelte
@@ -0,0 +1,19 @@
+<script>
+    import SubHeader from '../components/SubHeader.svelte';
+    import Menu from '../components/Menu.svelte';
+
+    let images = [
+        {
+            src: '/images/menus/dessert.png',
+            alt: 'Fikameny',
+        },
+    ];
+</script>
+
+<svelte:head>
+    <title>Fika - The Coffee Lounge</title>
+</svelte:head>
+
+<SubHeader
+    title="Fikameny" />
+<Menu {images} />
