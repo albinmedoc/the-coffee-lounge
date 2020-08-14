d96befd2b628e3420341706c1407ea40f96ab6eb
Albin Médoc <stamline203@gmail.com>
2020-08-14 23:45:13 +0200

    Created Ice Cream page

---
+++ src/routes/glass.svelte
@@ -0,0 +1,19 @@
+<script>
+    import SubHeader from '../components/SubHeader.svelte';
+    import Menu from '../components/Menu.svelte';
+
+    let images = [
+        {
+            src: '/images/menus/ice-cream.png',
+            alt: 'Glassmeny',
+        },
+    ];
+</script>
+
+<svelte:head>
+    <title>Glass - The Coffee Lounge</title>
+</svelte:head>
+
+<SubHeader
+    title="Glassmeny" />
+<Menu {images} />
