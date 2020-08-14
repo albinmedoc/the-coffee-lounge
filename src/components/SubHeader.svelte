d4e4d5e53dff0d494b99f950ed759818e06330d2
Albin Médoc <stamline203@gmail.com>
2020-08-14 23:37:14 +0200

    Created component for subheaders

---
+++ src/components/SubHeader.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let title;
+    export let description;
+</script>
+
+<style>
+    section {
+        text-align: center;
+    }
+
+    section > h3 {
+        font-size: 3rem;
+        margin: 10px 0;
+    }
+
+    section > p {
+        font-size: 1rem;
+    }
+</style>
+
+<section>
+    <h3>{title}</h3>
+    <p>{description}</p>
+</section>
