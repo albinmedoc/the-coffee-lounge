490be17631a72e23a1e3c635d7d8f4cf6a0a1c9e
Albin Médoc <stamline203@gmail.com>
2020-08-14 22:15:17 +0200

    Created Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,37 @@
+<style>
+    footer {
+        text-align: center;
+    }
+
+    footer > section > h4 {
+        font-size: 3rem;
+        margin: 45px 0 10px 0;
+    }
+
+    footer > section > iframe {
+        border: 0;
+        width: 100%;
+        max-width: 600px;
+        height: 350px;
+        margin: 0 auto;
+    }
+
+    footer > section > p {
+        font-size: 1.5rem;
+    }
+</style>
+
+<footer>
+    <section>
+        <h4>Hitta till oss</h4>
+        <iframe
+            title="Karta"
+            frameborder="0"
+            src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJUSF2f36ZU0YRTHlUZzVFPpQ&key=AIzaSyAnOfonfQ_n0gk9jiWRTYzfJwqzskwXBqI"
+            allowfullscreen />
+    </section>
+    <section>
+        <h4>Öppettider</h4>
+        <p>Måndag-Söndag: 11-18</p>
+    </section>
+</footer>
