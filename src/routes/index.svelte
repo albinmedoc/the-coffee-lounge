26f5b438c5d7999514390dae4630fb22dc6048b4
Albin Médoc <stamline203@gmail.com>
2020-08-14 22:25:21 +0200

    Created homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,135 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+    #info-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 90vh;
+        min-height: 90vh;
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+        background-image: url('/images/girl.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+    #info-container > div {
+        background: rgba(0, 0, 0, 0.8);
+        width: 90%;
+        max-width: 420px;
+        height: 30vh;
+        text-align: center;
+    }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+    #info-container > div > h4 {
+        font-weight: 700;
+        font-size: 1.8rem;
+    }
 
-	p {
-		margin: 1em auto;
-	}
+    #meny-container {
+        margin: 50px 0;
+    }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    #meny-container > .row {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+    }
+
+    #meny-container > .row > div {
+        margin-top: 15px;
+        height: 350px;
+        width: calc(50% - 30px);
+
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-origin: content-box;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #food-container {
+        background-image: url('/images/food.png');
+    }
+
+    #drink-container {
+        background-image: url('/images/drink.png');
+    }
+
+    #dessert-container {
+        background-image: url('/images/dessert.png');
+    }
+
+    #ice-cream-container {
+        background-image: url('/images/ice-cream.png');
+    }
+
+    #meny-container > .row > div > a {
+        background: rgba(0, 0, 0, 0.8);
+        width: 10rem;
+        height: 10rem;
+        text-decoration: none;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #meny-container > .row > div > a:hover {
+        background: rgba(0, 0, 0, 0.7);
+    }
+
+    #meny-container > .row > div > a > h4 {
+        font-size: 2rem;
+    }
+
+    @media (max-width: 800px) {
+        #meny-container > .row > div {
+            width: 100%;
+        }
+    }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+    <title>The Coffee Lounge</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<section id="info-container">
+    <div>
+        <h4>The Coffee Lounge</h4>
+        <h4>
+            Take Away
+            <br />
+            076-0047267
+        </h4>
+    </div>
+</section>
+<section id="meny-container">
+    <div class="row">
+        <div id="food-container">
+            <a href="/mat">
+                <h4>Mat</h4>
+            </a>
+        </div>
+        <div id="drink-container">
+            <a href="/dryck">
+                <h4>Dryck</h4>
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div id="dessert-container">
+            <a href="/fika">
+                <h4>Fika</h4>
+            </a>
+        </div>
+        <div id="ice-cream-container">
+            <a href="/glass">
+                <h4>Glass</h4>
+            </a>
+        </div>
+    </div>
+</section>
