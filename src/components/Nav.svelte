9610faa0d1e22c81383a22449ca2548d75bce996
Albin Médoc <stamline203@gmail.com>
2020-08-14 22:07:30 +0200

    Created Navigation

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,148 @@
 <script>
+    import HamburgerButton from './HamburgerButton.svelte';
 	export let segment;
+	
+	let active = false;
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+    header {
+        background-color: #000;
+        display: flex;
+        align-items: center;
+        flex-wrap: nowrap;
+        height: 164px;
+        padding: 0 32px;
+    }
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
+    .logo {
+        width: 100px;
+    }
 
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+    nav {
+        /* 100% - logo_size */
+        width: calc(100% - 100px);
+        z-index: 10;
+    }
 
-	li {
-		display: block;
-		float: left;
-	}
+    nav > :global(button) {
+        display: none;
+    }
 
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
+    nav > ul {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        list-style-type: none;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        text-align: center;
+    }
 
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
+    nav > ul > li > a {
+        text-decoration: none;
+        font-size: 1.5rem;
+        color: #b5b5b5;
+    }
+
+    [aria-current] {
+        position: relative;
+        display: inline-block;
+    }
+
+    [aria-current]::after {
+        position: absolute;
+        content: '';
+        width: 100%;
 		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
+		border-radius: 1px;
+        background-color: #b5b5b5;
+        display: block;
+        bottom: -1px;
+    }
 
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+    @media (max-width: 800px) {
+        nav {
+            position: absolute;
+            width: 100vw;
+            height: calc(100vh - 164px);
+            top: 164px;
+            left: 0;
+        }
+
+        nav > :global(button) {
+            display: block;
+            position: absolute;
+            top: -100px;
+            right: 32px;
+        }
+
+        nav > ul {
+            display: none;
+        }
+
+        nav.is-active > ul {
+            display: block !important;
+            background-color: #000;
+        }
+
+        nav > ul > li > a {
+            font-size: 2rem;
+        }
+    }
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
-</nav>
+<header>
+    <a href=".">
+        <img
+            class="logo"
+            src="/images/logo.png"
+            alt="The Coffee Lounge"
+            title="Startsida" />
+    </a>
+    <nav class="{active ? 'is-active' : ''}">
+		<HamburgerButton bind:active={active} />
+        <ul>
+            <li>
+                <a
+                    rel="prefetch"
+                    aria-current={segment === undefined ? 'page' : undefined}
+                    href="mat">
+                    Mat
+                </a>
+            </li>
+            <li>
+                <a
+                    rel="prefetch"
+                    aria-current={segment === 'dryck' ? 'page' : undefined}
+                    href="dryck">
+                    Dryck
+                </a>
+            </li>
+            <li>
+                <a
+                    rel="prefetch"
+                    aria-current={segment === 'fika' ? 'page' : undefined}
+                    href="fika">
+                    Fika
+                </a>
+            </li>
+            <li>
+                <a
+                    rel="prefetch"
+                    aria-current={segment === 'glass' ? 'page' : undefined}
+                    href="glass">
+                    Glass
+                </a>
+            </li>
+            <li>
+                <a
+                    aria-current={segment === 'kontakt' ? 'page' : undefined}
+                    href="kontakt">
+                    Kontakt
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
