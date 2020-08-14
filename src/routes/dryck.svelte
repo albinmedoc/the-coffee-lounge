42f4ecffce563af3cc552649c6817ed656f5c653
Albin Médoc <stamline203@gmail.com>
2020-08-14 23:41:12 +0200

    Created drinks page

---
+++ src/routes/dryck.svelte
@@ -0,0 +1,23 @@
+<script>
+    import SubHeader from '../components/SubHeader.svelte';
+    import Menu from '../components/Menu.svelte';
+
+    let images = [
+        {
+            src: '/images/menus/drinks-1.png',
+            alt: 'Dryckmeny 1',
+        },
+        {
+            src: '/images/menus/drinks-2.png',
+            alt: 'Dryckmeny 2',
+        },
+    ];
+</script>
+
+<svelte:head>
+    <title>Dryck - The Coffee Lounge</title>
+</svelte:head>
+
+<SubHeader
+    title="Dryckmeny" />
+<Menu {images} />
