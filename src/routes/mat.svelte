c73cddf19b41600eea60e84dc43a073286b82121
Albin Médoc <stamline203@gmail.com>
2020-08-14 23:37:57 +0200

    Created food page

---
+++ src/routes/mat.svelte
@@ -0,0 +1,24 @@
+<script>
+    import SubHeader from '../components/SubHeader.svelte';
+    import Menu from '../components/Menu.svelte';
+
+    let images = [
+        {
+            src: '/images/menus/food-1.png',
+            alt: 'Matmeny 1',
+        },
+        {
+            src: '/images/menus/food-2.png',
+            alt: 'Matmeny 2',
+        },
+    ];
+</script>
+
+<svelte:head>
+    <title>Mat - The Coffee Lounge</title>
+</svelte:head>
+
+<SubHeader
+    title="Matmeny"
+    description="Dagens lunch alla vardagar kl. 11-14, 79kr (inkl. bryggkaffe)." />
+<Menu {images} />
