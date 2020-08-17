54172b3b524ac4e938934bf03af47abb858a8075
Albin Médoc <stamline203@gmail.com>
2020-08-17 10:34:28 +0200

    Load envirement variables and use express.json()

--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ import sirv from 'sirv';
 import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+require('dotenv').config();
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 express()
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+    .use(express.json())
+    .use(
+        compression({ threshold: 0 }),
+        sirv('static', { dev }),
+        sapper.middleware()
+    )
+    .listen(PORT, (err) => {
+        if (err) console.log('error', err);
+    });
